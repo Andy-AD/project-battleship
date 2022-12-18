7174abb7c7a85438e8480615f0ecdd441251fdc1
Andrii Dovhaliuk <andy87@live.ru>
2022-12-18 11:12:41 +0400

    update shipFactory and test for it

--- src/shipFactory.js
+++ src/shipFactory.js
@@ -1,3 +1,12 @@
-export default function ship(length) {
-    return {length}
+export default function shipFactory(length) {
+  return {
+    length,
+    numberOfHits: 0,
+    isSunk: function () {
+      return this.length - this.numberOfHits > 0 ? false : true;
+    },
+    hit: function () {
+      this.numberOfHits++;
+    },
+  };
 }
--- src/shipFactory.test.js
+++ src/shipFactory.test.js
@@ -1,6 +1,26 @@
-import ship from "./shipFactory";
+import shipFactory from "./shipFactory";
 
-test('creates ship with length 3', () => {
-    expect(ship(3)).toStrictEqual({length: 3})
-})
-test()
+describe("new ship", () => {
+  let newShip = shipFactory(3);
+  test("length equals 3", () => {
+    expect(newShip.length).toBe(3);
+  });
+
+  test("numberOfHits equals 0", () => {
+    expect(newShip.numberOfHits).toBe(0);
+  });
+
+  test("isSunk equal to false", () => {
+    expect(newShip.isSunk()).toBe(false);
+  });
+
+  test("numberOfHits should equal to 1 after hit()", () => {
+    newShip.hit();
+    expect(newShip.numberOfHits).toBe(1);
+  });
+  test("isSunk() equals to true", () => {
+    newShip.hit();
+    newShip.hit();
+    expect(newShip.isSunk()).toBe(true);
+  });
+});
